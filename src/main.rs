bb317c0ace5a72e5fd30e1952db2715ab54eb9f9
fantasysk <lisankai93@qq.com>
2025-09-05 22:43:36 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use actix_web::{
 use actix_files as fs;
 use anyhow::{anyhow, Result};
 use chrono::{FixedOffset, TimeZone, Utc};
-use log::{debug, warn, error};
+use log::{debug, info, warn, error};
 use serde::Deserialize;
 use reqwest::Client;
 use std::{
